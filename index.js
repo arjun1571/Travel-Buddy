9b309f62d726e4ab2de742967285c42e9abb168c
Arjun Das <arjundasback515@gmail.com>
2022-08-18 22:21:15 +0600

    navigation and carosal added

---
+++ index.js
@@ -0,0 +1,37 @@
+// this is object section
+
+var carObject = {
+  vehicle: "Car",
+  imageUrl:
+    "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+
+  farePerKilo: 3,
+  capacity: 4,
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
+};
+
+var boatObject = {
+  vehicle: "Car",
+  imageUrl:
+    "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+
+  farePerKilo: 3,
+  capacity: 4,
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
+};
+var bikeObject = {
+  vehicle: "Bike",
+  imageUrl:
+    "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
+
+  farePerKilo: 2,
+
+  capacity: 2,
+
+  description:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
+};
+
+console.log("hello");
