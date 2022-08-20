25aa4383fc132a5ff2215b2c9d7ae7602eac6cee
Arjun Das <arjundasback515@gmail.com>
2022-08-20 21:20:57 +0600

    multiple card added and create dynamic modal

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var carObject = {
   imageUrl:
     "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
 
-  farePerKilo: 3,
-  capacity: 4,
+  farePerKilo: 2,
+  capacity: 2,
   description:
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
 };
 
-var boatObject = {
-  vehicle: "Car",
+var busObject = {
+  vehicle: "bus",
   imageUrl:
-    "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
 
   farePerKilo: 3,
   capacity: 4,
@@ -34,4 +34,82 @@ var bikeObject = {
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga eligendi veritatis expedita, dicta aut eum itaque ut tempora alias laudantium?",
 };
 
-console.log("hello");
+function displayServices(services) {
+  const mainSection = document.getElementById("main-content");
+  const stu = JSON.stringify(services);
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div class="card  mb-3 mx-auto " >
+          <div class="mt-10 row g-0">
+            <div class="col-md-4">
+              <img
+                src=${services.imageUrl}
+                class="img-fluid rounded-start h-100"
+                alt="..."
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Transport Mood:${services.vehicle}</h5>
+                <p class="card-text">
+                  ${services.description}
+                </p>
+                <p class="card-text">
+                    <small class="text-muted">Fare per kilo : ${services.farePerKilo}</small> <small class="text-muted">Capacity:${services.capacity}</small>
+                </p>
+                <!-- Button trigger modal -->
+                <button type="button" onclick='handleBook(${stu})' class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  Launch demo modal
+                </button>
+
+              </div>
+            </div>
+          </div>
+        </div>
+`;
+  mainSection.appendChild(div);
+
+}
+displayServices(carObject);
+displayServices(bikeObject);
+displayServices(busObject);
+
+// handle booking info 
+function handleBook(stu){
+  const modalBody = document.getElementById("modal-body");
+  modalBody.innerHTML = `
+  <div class="card mx-auto" style="width: 28rem;">
+  <img src="${stu.imageUrl}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Vehicle Mod : ${stu.vehicle}</h5>
+    <p class="card-text">${stu.description}</p>
+    <p class="card-text">
+      <small class="text-muted">Fare per kilo : ${stu.farePerKilo}</small> <small class="text-muted">Capacity:${stu.capacity}</small>
+    </p>
+    <div class="d-flex flex-column">
+      <input class="form-control m-2" type="search" placeholder="Search" aria-label="Search" />
+      <input class="form-control m-2" type="search" placeholder="Search" aria-label="Search" />
+      <input class="form-control m-2" type="search" placeholder="Search" aria-label="Search" />
+      <button class="btn btn-outline-success m-2" type="submit">
+       Search
+      </button>
+    </div>
+  </div>
+</div>
+  `
+
+// const div = document.createElement("div");
+// div.innerHTML=`
+//   <div class="card mx-auto" style="width: 28rem;">
+//   <img src="${stu.imageUrl}" class="card-img-top" alt="...">
+//   <div class="card-body">
+//     <h5 class="card-title">Vehicle Mod : ${stu.vehicle}</h5>
+//     <p class="card-text">${stu.description}</p>
+//     <a href="#" class="btn btn-primary">Go somewhere</a>
+//   </div>
+// </div>
+// `
+// modalBody.appendChild(div);
+
+
+}
